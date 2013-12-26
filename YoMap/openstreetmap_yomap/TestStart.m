2ef9b1f5f1535e631d6b10847dbc6d96dbfddac5
Nataia Shepeleva <natalia.shepel@gmail.com>
2013-12-26 14:11:09 +0100

    feel free to be happy now with TestStrat

--- YoMap/openstreetmap_yomap/TestStart.m
+++ YoMap/openstreetmap_yomap/TestStart.m
@@ -3,9 +3,6 @@ openstreetmap_filename = 'data/LeCreusotWaysFF.osm'; %'osm file with data';
 parsed_osm_filename = 'data/LeCreusotWaysFF.mat'; %'osm file with data';
 map_img_filename = 'data/map20000.png'; % image file saved from online, if available
 
-%any points
-xA =4.4076 ; yA =46.7844 ; xB = 4.4378; yB =46.8097 ;
-points = [xB xA; yB yA]
 
 %% convert XML -> MATLAB struct 
 %read and save to parsed_osm_filename
@@ -23,8 +20,9 @@ show_map(ax, parsed_osm.bounds, map_img_filename)
 plot_way(ax, parsed_osm)
 %plot_way(ax, parsed_osm,map_img_filename)
 
-[x,y] = ginput(2);
+[x,y] = ginput(2)
 
+points = [x(2) x(1); y(2) y(1)]
 
 %% plot routelayer
 route = findShortestWayByPosition(parsed_osm,x(1),y(1),x(2),y(2),1);
